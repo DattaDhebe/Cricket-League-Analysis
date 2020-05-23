4edfce72a06421f4c987bd7d9ea1c65e463118c1
DattaDhebe <dattadhebe75@gmail.com>
2020-05-23 23:37:36 +0000

    Refactored. Code to create Loader to load the Ipl File Data.

--- src/main/java/com/analyser/CricketAnalyser.java
+++ src/main/java/com/analyser/CricketAnalyser.java
@@ -1,17 +1,9 @@
 package com.analyser;
 
-import com.censusanalyser.CSVBuilderException;
-import com.censusanalyser.CSVBuilderFactory;
-import com.censusanalyser.ICSVBuilder;
 import com.google.gson.Gson;
-
-import java.io.IOException;
-import java.io.Reader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
+
 
 public class CricketAnalyser {
 
@@ -23,38 +15,13 @@ public class CricketAnalyser {
     }
 
     public int loadIplMostRunData(String csvFilePath) throws CricketAnalyserException {
-        return loadCricketData(csvFilePath, IplRunSheetCSV.class);
+        iplRunSheetMap = new IplFileLoader().loadCricketData(csvFilePath, IplRunSheetCSV.class);
+        return this.iplRunSheetMap.size();
     }
 
     public int loadIplMostWicketData(String csvFilePath) throws CricketAnalyserException {
-        return loadCricketData(csvFilePath, IplWicketCSV.class);
-    }
-
-    private <E> int loadCricketData(String csvFilePath, Class<E> iplCSVClass) throws CricketAnalyserException {
-        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
-            ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
-            Iterator<E> csvFileIterator = csvBuilder.getCSVFileIterator(reader, iplCSVClass);
-            Iterable<E> csvIterable=()->csvFileIterator;
-            if (iplCSVClass.getName().equals("com.analyser.IplRunSheetCSV")) {
-                StreamSupport.stream(csvIterable.spliterator(),false)
-                        .map(IplRunSheetCSV.class::cast)
-                        .forEach(iplRunsCSV -> iplRunSheetMap.put(iplRunsCSV.player,new IplSheetDAO(iplRunsCSV)));
-            } else if (iplCSVClass.getName().equals("com.analyser.IplWicketCSV")) {
-                StreamSupport.stream(csvIterable.spliterator(),false)
-                        .map(IplWicketCSV.class::cast)
-                        .forEach(iplRunsCSV -> iplRunSheetMap.put(iplRunsCSV.player,new IplSheetDAO(iplRunsCSV)));
-            }
-            return this.iplRunSheetMap.size();
-        } catch (IOException e) {
-            throw new CricketAnalyserException(e.getMessage(),
-                    CricketAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
-        } catch (CSVBuilderException e) {
-            throw new CricketAnalyserException(e.getMessage(),
-                    CricketAnalyserException.ExceptionType.NO_CENSUS_DATA);
-        } catch (RuntimeException e) {
-            throw new CricketAnalyserException(e.getMessage(),
-                    CricketAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
-        }
+        iplRunSheetMap = new IplFileLoader().loadCricketData(csvFilePath, IplWicketCSV.class);
+        return this.iplRunSheetMap.size();
     }
 
     public String loadSortingOnBattingAverage()  {
---
+++ src/main/java/com/analyser/IplFileLoader.java
@@ -0,0 +1,45 @@
+package com.analyser;
+
+import com.censusanalyser.CSVBuilderException;
+import com.censusanalyser.CSVBuilderFactory;
+import com.censusanalyser.ICSVBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.stream.StreamSupport;
+
+public class IplFileLoader {
+
+    public  <E> Map<String, IplSheetDAO> loadCricketData(String csvFilePath, Class<E> iplCSVClass) throws CricketAnalyserException {
+        Map<String, IplSheetDAO> iplRunSheetMap = new HashMap<>();
+        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
+            ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
+            Iterator<E> csvFileIterator = csvBuilder.getCSVFileIterator(reader, iplCSVClass);
+            Iterable<E> csvIterable=()->csvFileIterator;
+            if (iplCSVClass.getName().equals("com.analyser.IplRunSheetCSV")) {
+                StreamSupport.stream(csvIterable.spliterator(),false)
+                        .map(IplRunSheetCSV.class::cast)
+                        .forEach(iplRunsCSV -> iplRunSheetMap.put(iplRunsCSV.player,new IplSheetDAO(iplRunsCSV)));
+            } else if (iplCSVClass.getName().equals("com.analyser.IplWicketCSV")) {
+                StreamSupport.stream(csvIterable.spliterator(),false)
+                        .map(IplWicketCSV.class::cast)
+                        .forEach(iplRunsCSV -> iplRunSheetMap.put(iplRunsCSV.player,new IplSheetDAO(iplRunsCSV)));
+            }
+            return iplRunSheetMap;
+        } catch (IOException e) {
+            throw new CricketAnalyserException(e.getMessage(),
+                    CricketAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+        } catch (CSVBuilderException e) {
+            throw new CricketAnalyserException(e.getMessage(),
+                    CricketAnalyserException.ExceptionType.NO_CENSUS_DATA);
+        } catch (RuntimeException e) {
+            throw new CricketAnalyserException(e.getMessage(),
+                    CricketAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+        }
+    }
+}
