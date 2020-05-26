784c1a7d06c7070ca36ed5168b72b67585c74494
DattaDhebe <dattadhebe75@gmail.com>
2020-05-26 19:40:28 +0000

    Refactored. sortData modified to save time in compilation.

--- src/main/java/com/analyser/BattingAndBowlingAdapter.java
+++ src/main/java/com/analyser/BattingAndBowlingAdapter.java
@@ -5,20 +5,20 @@ import java.util.Map;
 
 public class BattingAndBowlingAdapter extends IplFileAdapter {
 
-    Map<String, IplSheetDAO> batsmanData = new HashMap<>();
-    Map<String, IplSheetDAO> bowlerData = new HashMap<>();
+    Map<String, IplSheetDAO> battingData = new HashMap<>();
+    Map<String, IplSheetDAO> bowlingData = new HashMap<>();
 
     @Override
     public Map<String, IplSheetDAO> loadCricketData(CricketAnalyser.Cricket cricket, String... csvFilePath) throws CricketAnalyserException {
-        batsmanData = super.loadCricketData(IplRunSheetCSV.class, csvFilePath[0]);
-        bowlerData = super.loadCricketData(IplWicketCSV.class, csvFilePath[1]);
+        battingData = super.loadCricketData(IplRunSheetCSV.class, csvFilePath[0]);
+        bowlingData = super.loadCricketData(IplWicketCSV.class, csvFilePath[1]);
 
-        bowlerData.values().stream()
-                            .filter(iplLeague -> batsmanData.get(iplLeague.player) != null)
+        bowlingData.values().stream()
+                            .filter(iplLeague -> battingData.get(iplLeague.player) != null)
                             .forEach(iplLeague -> {
-                                batsmanData.get(iplLeague.player).bowlingAverage = iplLeague.bowlingAverage;
-                                batsmanData.get(iplLeague.player).wickets = iplLeague.wickets;
+                                battingData.get(iplLeague.player).bowlingAverage = iplLeague.bowlingAverage;
+                                battingData.get(iplLeague.player).wickets = iplLeague.wickets;
                             });
-        return batsmanData;
+        return battingData;
     }
 }
